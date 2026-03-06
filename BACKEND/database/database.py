2cefcd490f78263546988117fa6fcac135eecbd3
Nandhana <nandhanarajan03@gmail.com>
2026-03-06 12:01:34 +0530

    Switch database to SQLite for deployment

--- BACKEND/database/database.py
+++ BACKEND/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:newpassword123@localhost:5432/multi_agent_ai"
+DATABASE_URL = "sqlite:///./decision_logs.db"
 
 engine = create_engine(DATABASE_URL)
 
