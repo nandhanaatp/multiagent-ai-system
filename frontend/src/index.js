f08485508ade254d67015a17059f0f62f0f4062b
Nandhana <nandhanarajan03@gmail.com>
2026-04-15 03:06:26 +0530

    feat: convert to PWA - manifest, service worker, meta tags

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,13 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('SW registered'))
+      .catch(() => console.log('SW registration failed'));
+  });
+}
+
 reportWebVitals();
